b91c295002a1c0b02a33fd10857072e6fdfe29b8
Michael Drennen <michaeldrennen74@gmail.com>
2025-09-08 08:26:00 -0600

    Updated the logger interface.

--- src/DebugLogger.php
+++ src/DebugLogger.php
@@ -4,7 +4,6 @@ namespace DPRMC\GLMXFixClient;
 
 class DebugLogger implements LogInterface {
 
-
     public function logParsed( array $content ) {
         print_r( $content );
     }
@@ -13,6 +12,13 @@ class DebugLogger implements LogInterface {
         echo $message . "\n";
     }
 
+
+    /**
+     * @param \DPRMC\GLMXFixClient\FixMessage $fixMessage
+     * @param array                           $meta
+     *
+     * @return void
+     */
     public function log( FixMessage $fixMessage, array $meta = [] ) {
         print_r($fixMessage);
         print_r($meta);
