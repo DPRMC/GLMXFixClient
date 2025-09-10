1983dca24954761037a892d4cdc1a2546c35eabf
Michael Drennen <michaeldrennen74@gmail.com>
2025-09-10 15:25:14 -0600

    Updated the interface.

--- src/FixMessageRepositoryInterface.php
+++ src/FixMessageRepositoryInterface.php
@@ -11,13 +11,21 @@ interface FixMessageRepositoryInterface {
 
 
     /**
+     * @param string $host
+     * @param int    $port
      * @param Carbon $date
-     * @param int $msgSeqNumStart
-     * @param int $msgSeqNumEnd Ex: if 0, then send all messages from msgSeqNumStart to the end for the given ordinal day.
-     * @param int $direction Ex: -1 for outgoing, 1 for incoming, 0 for both directions
+     * @param int    $msgSeqNumStart
+     * @param int    $msgSeqNumEnd Ex: if 0, then send all messages from msgSeqNumStart to the end for the given ordinal day.
+     * @param int    $direction    Ex: -1 for outgoing, 1 for incoming, 0 for both directions
+     *
      * @return array
      */
-    public function getMessagesBetweenMsgSeqNums(Carbon $date, int $msgSeqNumStart, int $msgSeqNumEnd, int $direction = -1 ): array;
+    public function getMessagesBetweenMsgSeqNums(string $host,
+                                                 int $port,
+                                                 Carbon $date,
+                                                 int $msgSeqNumStart,
+                                                 int $msgSeqNumEnd,
+                                                 int $direction = -1 ): array;
 
 
 
